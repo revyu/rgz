439700d6211221d825318131e5add85fa6744451
Sergey <revyakin8800@gmail.com>
2024-05-12 16:41:33 +0700

    added new tests

--- tests.py
+++ tests.py
@@ -1,14 +1,18 @@
 import os
 
-name="file.txt"
 with open("file.txt", "w") as file:
     
-    file.write("eto osnovnoi potok\n")
+    file.write("this is main stream\n")
    
-    os.system("echo eto dopolnitelnii potok > file.txt:stream_name")
+    os.system("echo this is extended stream > file.txt:stream_name")
     os.system("echo 1 >file.txt:stream_name2")
 
     
 with open("file_no_ext_streams.txt", "w") as file:
     
-    file.write("eto osnovnoi potok\n")
+    file.write("this is main stream\n")
+
+with open("file_no_main_stream.txt", "w") as file:
+    
+    os.system("echo this is extended stream > file_no_main_stream.txt:stream_name")
+    
